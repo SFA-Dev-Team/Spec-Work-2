12544b8900bfffa273190a6ab28b77f6731c4a1b
SFA-Dev-Team <web@sfamarketing.com>
2025-06-12 13:51:15 -0400

    Updated code

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,10 +7,10 @@ export default function Header() {
         <img src="./ninth-street-capital.png" alt="Ninth Street Capital Partners Logo" />
       </div>
       <nav className="nav">
-        <a href="#">Who we are</a>
-        <a href="#">Our Approach</a>
-        <a href="#">About Us</a>
-        <a href="#">Investor Login</a>
+        <a href="">Who we are</a>
+        <a href="">Our Approach</a>
+        <a href="">About Us</a>
+        <a href="">Investor Login</a>
       </nav>
     </header>
   );
