e639ccaaf684901ee3ed80dd83efe49b963f4902
SFA-Dev-Team <web@sfamarketing.com>
2025-06-23 15:07:54 -0400

    updated code

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -1,5 +1,7 @@
 import { motion } from 'framer-motion';
 import './sec1.css';
+import b1 from '../assets/b4.jpg';
+import b2 from '../assets/b2.jpg';
 
 export default function Section1() {
   return (
@@ -11,11 +13,13 @@ export default function Section1() {
         transition={{ duration: 0.8, ease: "easeOut" }}
         whileHover={{ scale: 1.03, boxShadow: "0 8px 16px rgba(0,0,0,0.2)" }}
       >
-        <h2>NSCP Investment Criteria</h2>
-        <p>
-          We invest in businesses with niche leadership positions. When evaluating possible investments, the most important criteria we look for is differentiation: what is it about a prospect that provides superior long-term opportunities. We also thoroughly consider financial, industry and company-specific criteria.
-        </p>
-        <button>Learn More</button>
+          <img src={b1} alt="Investment Criteria" className="box-image" />
+          <h2>NSCP Investment Criteria</h2>
+          <p>
+            We invest in businesses with niche leadership positions. When evaluating possible investments, the most important criteria we look for is differentiation: what is it about a prospect that provides superior long-term opportunities. We also thoroughly consider financial, industry and company-specific criteria.
+          </p>
+          <button>Learn More</button>
+        
       </motion.div>
 
       <motion.div
@@ -25,6 +29,7 @@ export default function Section1() {
         transition={{ duration: 0.8, delay: 0.3, ease: "easeOut" }}
         whileHover={{ scale: 1.03, boxShadow: "0 8px 16px rgba(0,0,0,0.2)" }}
       >
+        <img src={b2} alt="Investment Criteria" className="box-image" />
         <h2>NSCP Value Proposition</h2>
         <p>
           <strong>For business owners</strong> seeking an exit or liquidity, NSCP structures flexible solutions.
